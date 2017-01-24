a107c89d5bbe0183fbbbec9908f7529dadb18596
Pietro Grandi <pietro.grandi@evokegaming.com>
2017-01-24 22:12:13 +0100

    Landing page with jade ready.

--- server/server.js
+++ server/server.js
@@ -4,26 +4,19 @@ var morgan = require("../node_modules/morgan");
 var db = require("./db.js");
 var app = express();
 
-// Set the port
 app.set("port", process.env.PORT || 3000);
-// Set the server working directory
 app.use(express.static(__dirname + './../public'));
-// log every request to the console
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-//app.engine('jade', ejs.renderFile);
-//app.set('view engine', 'html');
-app.set('views', './views');
+app.set('views', './server/views');
 app.set('view engine', 'pug');
-
-
 app.use(bodyParser.json());
 
 var routes = require('./routes/routes');
-// Routes using Express
 routes(app);
+
 // App start
 app.listen(app.get("port"), function () {
     console.log("Express server listening on port " + app.get("port"));
